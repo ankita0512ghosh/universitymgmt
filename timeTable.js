7ab22053dd7dd5f9d8005ff456f0396cdad5a93e
Ankita Ghosh <ankita0512ghosh@gmail.com>
2024-04-15 21:24:05 +0530

    university mgmt

---
+++ timeTable.js
@@ -0,0 +1,133 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '11 AM - 12 PM',
+        roomNumber: 'E-310',
+        subject: 'WN',
+        type: 'Lecture'
+    },
+    {   
+        time: '12 PM - 01 PM',
+        roomNumber: 'E-206',
+        subject: 'MP',
+        type: 'Lecture'
+    },
+    {   
+        time: '01 PM - 02 PM',
+        roomNumber: 'E-206',
+        subject: 'CD',
+        type: 'Lecture'
+    },
+    {   
+        time: '03 PM - 05 PM',
+        roomNumber: 'DBMS Lab',
+        subject: 'CD',
+        type: 'Lab'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09 AM - 11 AM',
+        roomNumber: 'DBMS Lab',
+        subject: 'MT',
+        type: 'Lab'
+    },
+    {   
+        time: '12 PM - 01 PM',
+        roomNumber: 'E-206',
+        subject: 'CC',
+        type: 'Lecture'
+    },
+    {   
+        time: '01 PM - 03 PM',
+        roomNumber: 'DIC Lab',
+        subject: 'MP',
+        type: 'Lab'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '11 AM - 01 PM',
+        roomNumber: 'Networking Lab',
+        subject: 'WN',
+        type: 'Lab'
+    },
+    {   
+        time: '01 PM - 02 PM',
+        roomNumber: 'E-206',
+        subject: 'MT',
+        type: 'Lecture'
+    },
+    {   
+        time: '02 PM - 03 PM',
+        roomNumber: 'E-206',
+        subject: 'MP',
+        type: 'Lecture'
+    },
+    {   
+        time: '03 PM - 05 PM',
+        roomNumber: 'E-206',
+        subject: 'FM',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11 AM - 01 PM',
+        roomNumber: 'E-206',
+        subject: 'CD',
+        type: 'Lecture'
+    },
+    {   
+        time: '02 PM - 03 PM',
+        roomNumber: 'E-206',
+        subject: 'CC',
+        type: 'Lecture'
+    },
+    {   
+        time: '03 PM - 05 PM',
+        roomNumber: 'E-206',
+        subject: 'MT',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '11 AM - 12 PM',
+        roomNumber: 'E-206',
+        subject: 'MP',
+        type: 'Lecture'
+    },
+    {   
+        time: '01 PM - 03 PM',
+        roomNumber: 'E-310',
+        subject: 'WN',
+        type: 'Lecture'
+    },
+    {   
+        time: '03 PM - 05 PM',
+        roomNumber: 'E-310',
+        subject: 'CC',
+        type: 'Lecture'
+    }
+
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
